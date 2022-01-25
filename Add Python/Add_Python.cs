dad596dd11b63f24038c67dc8fa190f1db840cd4
Aviv Cohn <ACohn@DGITech.com>
2022-01-25 14:18:40 -0500

    init

---
+++ Add Python/Add_Python.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Add_Python
+{
+    internal class Add_Python
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AppDomain.CurrentDomain.BaseDirectory);
+        }
+    }
+}
